64be822b7107dfc68a962e91e9c757b76853a881
SakilaWAW <pccp0909@163.com>
2018-04-23 11:16:27 +0800

    更新注释和任务时间以及任务设置提示

--- scripts/auto_injection_files.js
+++ scripts/auto_injection_files.js
@@ -1,19 +1,21 @@
-// 本脚本用于在每天晚上自动检测新增文章并将文章内容注入数据库
+/**
+ * 本脚本用于设置定时任务:在每天晚上自动检测新增文章并将文章内容注入数据库
+ */
+const FILE_PATH = '/home/stg/WebProjects/md_files/';
 
 const schedule = require("node-schedule");
 const inject_executor = require('./file_inject_executor');
 
 let rule = new schedule.RecurrenceRule();
-const md_file_path = '/home/stg/WebProjects/md_files/';
 
 rule.dayOfWeek = [0, new schedule.Range(1, 6)];
-rule.hour = 19;
-rule.minute = 52;
+rule.hour = 2;
+rule.minute = 30;
 
 schedule.scheduleJob(rule, function(){
-  inject_executor.exec(md_file_path).then(()=>{
+  inject_executor.exec(FILE_PATH).then(()=>{
     console.log(`任务于:${new Date()}  执行...`);
   });
 });
 
-console.log("定时任务设置完毕");
+console.log(`定时任务设置完毕--定时任务时间为:每日${rule.hour}时${rule.minute}分${rule.second}秒`);
