7bb37390e5dfb4ef2e9a65ca88d984a5b5dfd6e5
SakilaWAW <pccp0909@163.com>
2018-05-04 13:54:54 +0800

    将跨域所需的参数提取到koa2公共流程中

--- controller/article.js
+++ controller/article.js
@@ -2,8 +2,10 @@
 
 const db_util = require('../scripts/db_util');
 
-const _add_response_cors = async (ctx, next) => {
-  ctx.response.header;
+const _add_cors_tags = async (ctx, next) => {
+  ctx.response.set('Access-Control-Allow-Origin', '*');
+  ctx.response.set("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+  ctx.response.set("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
   await next();
 };
 
@@ -14,9 +16,6 @@ const _get_article_content = async (ctx) => {
 const _get_articles_by_page = async (ctx) => {
   const total_num = await db_util.queryArticleNum();
   let articles = await db_util.queryArticleByPage(ctx.params.page, 10);
-  ctx.response.set('Access-Control-Allow-Origin', '*');
-  ctx.response.set("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-  ctx.response.set("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
   ctx.body = {
     articles,
     total_page: Math.ceil(total_num/10),
@@ -59,16 +58,22 @@ const _get_tag_stats = async (ctx) => {
   ctx.body = await db_util.queryTagsGroups();
 };
 
+const _read_article = async (ctx) => {
+  await db_util.updateCount(ctx.params.id, 1);
+  ctx.response.status = 204;
+};
+
 /**
  * 传入规则为：
  * 属性名为方式和url路径的拼接
  * 属性值为一个数组,数组为一个处理流
  */
 module.exports = {
-  'GET /article/:id': [_get_article_content],
-  'GET /page/:page': [_get_articles_by_page],
-  'GET /type/:type': [_get_articles_of_type],
-  'GET /tag/:tag': [_get_articles_of_tag],
-  'GET /stats/article': [_get_article_stats],
-  'GET /stats/tag': [_get_tag_stats],
+  'GET /article/:id': [_add_cors_tags, _get_article_content],
+  'GET /page/:page': [_add_cors_tags, _get_articles_by_page],
+  'GET /type/:type': [_add_cors_tags, _get_articles_of_type],
+  'GET /tag/:tag': [_add_cors_tags, _get_articles_of_tag],
+  'GET /stats/article': [_add_cors_tags, _get_article_stats],
+  'GET /stats/tag': [_add_cors_tags, _get_tag_stats],
+  'GET /read/:id': [_add_cors_tags, _read_article],
 };
